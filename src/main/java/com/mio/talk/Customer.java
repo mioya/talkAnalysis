d8f775974fd8a8f4a99d08f7d8384fd3048a2262
mio <powalsld@naver.com>
2019-03-02 19:34:29 +0900

    초기값 세팅

---
+++ src/main/java/com/mio/talk/Customer.java
@@ -0,0 +1,27 @@
+package com.mio.talk;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+@Data
+@NoArgsConstructor
+public class Customer {
+
+    @Id
+    @GeneratedValue
+    private Long id;
+
+    private String firstName;
+
+    private String lastName;
+
+    public Customer(String firstName, String lastName){
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+}

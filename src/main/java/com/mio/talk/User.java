43f85da52374914775f50f657d3c406fbd07aa94
mio <powalsld@naver.com>
2019-03-04 00:46:22 +0900

    데이터베이스 h2->mysql

--- src/main/java/com/mio/talk/User.java
+++ src/main/java/com/mio/talk/User.java
@@ -6,10 +6,12 @@ import lombok.NoArgsConstructor;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.Table;
 
 @Entity
 @Data
 @NoArgsConstructor
+@Table(name = "users")
 public class User {
 
     @Id

a84c98cbcae86c69465420d83782cd7f4f7f3499
mio <powalsld@naver.com>
2019-03-07 21:00:32 +0900

    bootStrap 추가

--- src/main/java/com/mio/talk/UserController.java
+++ src/main/java/com/mio/talk/UserController.java
@@ -1,19 +1,22 @@
 package com.mio.talk;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
 
-@RestController
+@Controller
 public class UserController {
 
     @Autowired
     public UserRepository userRepository;
 
     @GetMapping(path = "/")
-    public List<User> talk(){
-        return userRepository.findAll();
+    public String talk(Model model){
+        model.addAttribute("users",userRepository.findAll());
+        return "/index";
     }
 }
